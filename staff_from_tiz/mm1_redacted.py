02f7d49a6841273c3e94ce470ef2bb7a9968bc83
Tiziano <40208107+Firpo7@users.noreply.github.com>
2021-02-03 19:27:36 +0100

    staff from tiz

---
+++ staff_from_tiz/mm1_redacted.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+
+from collections import deque
+from heapq import heappush, heappop
+from random import expovariate
+
+MAXT = 1000000
+LAMBDA = 0.7
+
+
+class Arrival:
+  def __init__(self, id):
+    self.id = id
+
+  def process(self, state):
+    # * save job arrival time in state.arrivals
+    # * push the new job arrival in the event queue
+    #   (the new event will happen at time t + expovariate(LAMBDA))
+    # * if the server FIFO queue is empty, add this job termination
+    #   (termination will happen at time t + expovariate(1))
+
+    state.arrivals[self.id] = state.t
+    
+    tmp_t = state.t + expovariate(state.LAMBDA)
+
+    # I moved this check here so that when the time finish I will not add events anymore
+    # but I will finish the ones in the queue, otherwise I would have Arrivals > Completions (nothing too bad btw)
+    if tmp_t < state.MAXT:
+      heappush(state.events, (tmp_t, Arrival(self.id + 1)))
+
+    if not len(state.fifo):
+      heappush(state.events, (state.t + expovariate(1), Completion(self.id)))
+    
+    state.fifo.append(self.id)
+
+
+class Completion:
+  def __init__(self, id):
+    self.id = id
+  
+  def process(self, state):
+    # * remove the first job from the FIFO queue
+    # * update its completion time in state.completions
+    # * insert the termination event for the next job in queue
+    tmp = state.fifo.popleft()
+    state.completions[tmp] = state.t
+
+    # check if it was not the last element
+    if len(state.fifo):
+      heappush(state.events, (state.t + expovariate(1), Completion(state.fifo[0])))
+
+
+class State:
+  def __init__(self, LAMBDA, MAXT):
+    self.t = 0  # current time in the simulation
+    self.events = [(0, Arrival(0))]  # queue of events to simulate
+    self.fifo = deque()  # queue at the server
+    self.arrivals = {}  # jobid -> arrival time mapping
+    self.completions = {}  # jobid -> completion time mapping
+    self.LAMBDA = LAMBDA
+    self.MAXT = MAXT
+
+
+def start(LAMBDA, MAXT):
+  state = State(LAMBDA, MAXT)
+  events = state.events
+
+  while events:
+    t, event = heappop(events)
+    # if t > MAXT:
+    #   break
+    state.t = t
+    event.process(state)
+  
+  return state
+
+state = start(LAMBDA, MAXT)
+
+deltas = {}
+values_sum = 0
+values_count = 0
+for k_arr, v_arr in state.arrivals.items():
+  dt = state.completions[k_arr] - v_arr
+  deltas[k_arr] = dt
+  values_sum += dt
+  values_count += 1
+
+mean = values_sum / values_count
+
+print(f"Mean time is : {mean}")
+print(f"Mean time expected is : {1/(1-LAMBDA)}")
+
+# process state.arrivals and state.completions, find average time spent
+# in the system, and compare it with the theoretical value of 1 / (1 - LAMBDA)
